06bfedd47063a3c7d58ef21c92638877c511d4e5
User <User@DESKTOP-TJEQVPK>
2025-05-19 19:39:19 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public interface IPrice
+{
+    double GetPrice();
+}
+
+public class Car : IPrice
+{
+    public double Price { get; set; }
+    public string Name { get; set; }
+
+    public Car(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public double GetPrice()
+    {
+        return Price;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car("Toyota", 30000);
+        Console.WriteLine($"車名: {myCar.GetName()}");
+        Console.WriteLine($"價格: {myCar.GetPrice()}");
+    }
+}
